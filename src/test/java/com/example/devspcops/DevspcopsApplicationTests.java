416b9025504048edf08bedbbf076a277f00206e3
BreeZe6167 <chin2bil@gmail.com>
2023-11-22 13:42:23 +0800

    first commit

---
+++ src/test/java/com/example/devspcops/DevspcopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devspcops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevspcopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
